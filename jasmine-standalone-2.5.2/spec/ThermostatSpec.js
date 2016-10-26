161881a7d37e9248371f68a822840c075d403b7b
Stuart Cooper <stucooper86@gmail.com>
2016-10-26 17:45:16 +0100

    Adding complete challenge 5

--- jasmine-standalone-2.5.2/spec/ThermostatSpec.js
+++ jasmine-standalone-2.5.2/spec/ThermostatSpec.js
@@ -1,27 +1,67 @@
 describe ("Thermostat", function() {
 
-});
+  beforeEach(function() {
+     thermostat = new Thermostat()
+  });
 
-describe ("New Thermostat", function(){
-  it("Has a START_TEMP of 20", function(){
-    var thermostat = new Thermostat()
-    expect(thermostat.startTemp).toBe(20)
+  describe ("New Thermostat", function(){
+    it("Has a START_TEMP of 20", function(){
+      expect(thermostat.startTemp).toBe(20)
+    });
+    it("has a TEMP of 20", function(){
+      expect(thermostat.temperature).toBe(thermostat.startTemp)
+    });
+    it("has a maxTemp of 25", function(){
+      expect(thermostat.maxTemp).toBe(25)
+    });
   });
-  it("has a TEMP of 20", function(){
-    var thermostat = new Thermostat()
-    expect(thermostat.temperature).toBe(thermostat.startTemp)
-  });
-});
 
-describe ("Change Temperature", function(){
-  it("increases temperature with upTemp()", function(){
-    var thermostat = new Thermostat()
-    thermostat.upTemp()
-    expect(thermostat.temperature).toBeGreaterThan(thermostat.startTemp)
+  describe ("Change Temperature", function(){
+    it("increases temperature with upTemp()", function(){
+      thermostat.upTemp()
+      expect(thermostat.temperature).toBeGreaterThan(thermostat.startTemp)
+    });
+    it("decreases temperature with downTemp()", function(){
+      thermostat.downTemp()
+      expect(thermostat.temperature).toBeLessThan(thermostat.startTemp)
+    });
   });
-  it("decreases temperature with downTemp()", function(){
-    var thermostat = new Thermostat()
-    thermostat.downTemp()
-    expect(thermostat.temperature).toBeLessThan(thermostat.startTemp)
+
+  describe ("Power Saving mode", function(){
+    it("sets powerSaveMode to true", function(){
+      thermostat.powerSaveOn()
+      expect(thermostat.powerSaveMode).toEqual(true)
+    });
+    it("sets maxTemp to 25 if powerSaveMode is true", function(){
+      thermostat.powerSaveOn()
+      expect(thermostat.maxTemp).toBe(25)
+    });
+    it("sets powerSaveMode to false", function(){
+      thermostat.powerSaveOff()
+      expect(thermostat.powerSaveMode).toEqual(false)
+    });
+  });
+
+  describe ("Reset", function(){
+    it("resets the temperature to 20 on reset", function(){
+      thermostat.upTemp()
+      thermostat.reset()
+      expect(thermostat.temperature).toEqual(thermostat.startTemp)
+    });
+  });
+
+  describe ("Energy usage", function(){
+    it("returns low energy use statement based on temperature", function(){
+      thermostat.temperature = 17
+      expect(thermostat.energyUse).toMatch("low-usage")
+    });
+    it("returns medium energy use statement based on temperature", function(){
+      thermostat.temperature = 24
+      expect(thermostat.energyUse).toMatch("medium-usage")
+    });
+    it("returns high energy use statement based on temperature", function(){
+      thermostat.temperature = 28
+      expect(thermostat.energyUse).toMatch("high-usage")
+    });
   });
 });
--- jasmine-standalone-2.5.2/src/Thermostat.js
+++ jasmine-standalone-2.5.2/src/Thermostat.js
@@ -1,6 +1,8 @@
 function Thermostat(){
   this.startTemp = 20;
   this.temperature = null || this.startTemp;
+  this.maxTemp = 25;
+  this.powerSaveMode = true;
 };
 
 Thermostat.prototype.upTemp = function (n) {
@@ -9,4 +11,32 @@ Thermostat.prototype.upTemp = function (n) {
 
 Thermostat.prototype.downTemp = function (n) {
   this.temperature--
+};
+
+Thermostat.prototype.powerSaveOn = function () {
+  this.setMaxTemp(25)
+  this.powerSaveMode = true
+};
+
+Thermostat.prototype.powerSaveOff = function () {
+  this.setMaxTemp(32)
+  this.powerSaveMode = false
+};
+
+Thermostat.prototype.setMaxTemp = function (n) {
+  this.maxTemp = n
+};
+
+Thermostat.prototype.reset = function () {
+  this.temperature = this.startTemp
+};
+
+Thermostat.prototype.energyUse = function () {
+  if (this.temperature < 18) {
+    return "low-usage" }
+  else if (this.temperature < 25) {
+    return "medium-usage" }
+  else {
+    return "high-usage"
+  }
 };
